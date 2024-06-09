09ce9a20d207a71ee8b4070a4da4bb3de23d8936
Djonko <prevostdjonko@yahoo.fr>
2024-06-09 00:45:31 -0400

    add tests

--- src/test/java/dev/lydtech/tracking/handler/DispatchPreparedHandlerTest.java
+++ src/test/java/dev/lydtech/tracking/handler/DispatchPreparedHandlerTest.java
@@ -1,23 +1,38 @@
 package dev.lydtech.tracking.handler;
 
+import dev.lydtech.tracking.message.DispatchPrepared;
+import dev.lydtech.tracking.service.TrackingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class DispatchPreparedHandlerTest {
+    private TrackingService trackingService;
+    private DispatchPreparedHandler dispatchPreparedHandler;
+
 
     @BeforeEach
     void setUp() {
+        trackingService = mock(TrackingService.class);
+        dispatchPreparedHandler= new DispatchPreparedHandler(trackingService);
     }
 
     @Test
-    void listen_Success() {
-        assertEquals(1,0);
+    void listen_Success() throws ExecutionException, InterruptedException {
+        DispatchPrepared payload = DispatchPrepared.builder().orderId(UUID.randomUUID()).build();
+        dispatchPreparedHandler.listen(payload);
+        verify(trackingService, times(1)).updateTrackingStatus(any(DispatchPrepared.class));
     }
 
     @Test
-    void listen_ThrowsException() {
-        assertEquals(1,0);
+    void listen_ThrowsException() throws ExecutionException, InterruptedException {
+        DispatchPrepared payload = DispatchPrepared.builder().orderId(UUID.randomUUID()).build();
+        dispatchPreparedHandler.listen(payload);
+        doThrow(new RuntimeException("update tracking failed")).when(trackingService).updateTrackingStatus(payload);
     }
 }
--- src/test/java/dev/lydtech/tracking/service/TrackingServiceTest.java
+++ src/test/java/dev/lydtech/tracking/service/TrackingServiceTest.java
@@ -1,18 +1,36 @@
 package dev.lydtech.tracking.service;
 
+import dev.lydtech.tracking.message.DispatchPrepared;
+import dev.lydtech.tracking.message.TrackingStatusUpdated;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.mockito.Mockito.*;
 
 class TrackingServiceTest {
+    private TrackingService trackingService;
+    private KafkaTemplate kafkaProducerMock;
+    private CompletableFuture completableFuture;
 
     @BeforeEach
     void setUp() {
+        kafkaProducerMock = mock(KafkaTemplate.class);
+        completableFuture = CompletableFuture.completedFuture(mock(SendResult.class));
+        trackingService = new TrackingService(kafkaProducerMock);
     }
 
     @Test
-    void updateTrackingStatus_Success() {
-        assertEquals(1,0);
+    void updateTrackingStatus_Success() throws ExecutionException, InterruptedException {
+        when(kafkaProducerMock.send(anyString(), any(TrackingStatusUpdated.class))).thenReturn(completableFuture);
+
+        DispatchPrepared dispatchPrepared = DispatchPrepared.builder().orderId(UUID.randomUUID()).build();
+        trackingService.updateTrackingStatus(dispatchPrepared);
+        verify(kafkaProducerMock, times(1)).send(eq(TrackingService.TRACKING_STATUS_TOPIC), any(TrackingStatusUpdated.class));
     }
 }
